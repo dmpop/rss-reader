4e0737c227ca512c1806c0a83216492aad821d8f
Dmitri Popov <dmpop@linux.com>
2021-12-29 22:45:47 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# RSS reader
+
+Simple PHP-based RSS aggregator.
+
+## Author
+
+Dmitri Popov [dmpop@linux.com](mailto:dmpop@linux.com)
+
+## License
+
+The [GNU General Public License version 3](http://www.gnu.org/licenses/gpl-3.0.en.html)
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+$theme = "light";
+$title = "RSS reader";
+$footer = "I really 🧡 <a href='https://www.paypal.com/paypalme/dmpop'>coffee</a>";
+$feeds = array(
+	"https://tokyoma.de/rss.xml",
+	"http://dpreview.com/feeds/news/latest",
+	"https://www.tomshardware.com/feeds/all",
+	"https://changelog.com/feed/",
+	"https://blog.linuxmint.com/?feed=rss2",
+	"https://www.omgubuntu.co.uk/feed"
+);
+?>
+
+<!DOCTYPE html>
+<html lang="en" data-theme="<?php echo $theme; ?>">
+
+<!-- Author: Dmitri Popov, dmpop@linux.com
+	 License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
+
+<head>
+	<title><?php echo $title; ?></title>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/classless.css" />
+	<link rel="stylesheet" href="css/themes.css" />
+	<!-- Suppress form re-submit prompt on refresh -->
+	<script>
+		if (window.history.replaceState) {
+			window.history.replaceState(null, null, window.location.href);
+		}
+	</script>
+</head>
+
+<body>
+	<div class="card text-center">
+		<div style="margin-top: 1em; margin-bottom: 1em;">
+			<img style="display: inline; height: 2.5em; vertical-align: middle;" src="favicon.svg" alt="logo" />
+			<h1 style="display: inline; margin-top: 0em; vertical-align: middle; letter-spacing: 3px;"><?php echo $title; ?></h1>
+		</div>
+		<hr style="margin-bottom: 2em;">
+		<?php
+		$array_length = count($feeds);
+		for ($i = 0; $i < $array_length; $i++) {
+			echo "<details>";
+			$rss = simplexml_load_file($feeds[$i]);
+			echo '<summary>' . $rss->channel->title . '</summary>';
+			echo "<ul>";
+			foreach ($rss->channel->item as $item) {
+				echo '<li style="font-size: 85%"><a href="' . $item->link . '">' . $item->title . "</a></li>";
+			}
+			echo "</ul>";
+			echo "</details>";
+		}
+		?>
+	</div>
+	<div class="text-center">
+		<?php echo $footer; ?>
+	</div>
+</body>
+
+</html>
